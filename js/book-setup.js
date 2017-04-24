812df9ae85c291a6c3fcf21e766c42b32bbc3a42
LorilynnKing-HP\Lorilynn King <lorilynnking@comcast.net>
2017-04-24 12:04:47 -0600

    Framework done and tested

--- js/book-setup.js
+++ js/book-setup.js
@@ -1,3 +1,7 @@
+function test(){
+  console.log("test");   // type test(); in console to call this to ensure we're communication with this js file
+};
+
 function newBook(title, author, pgcount, publishDt) {
   this.title = title;
   this.author = author;
@@ -5,17 +9,21 @@ function newBook(title, author, pgcount, publishDt) {
   this.publishDt = publishDt;
 };
 
-var library(){
-};
+var library = function(){};
 
 library.prototype.myBookArray = [];   // set up method add books to array
                         // or = new array();
-library.prototype.addBook =  // method to add book to array
-library.prototype.removeBookByTitle	= //(title)
-library.prototype.removeBookByAuthor	= //(authorName)
-library.prototype.getRandomBook	= //()
-library.prototype.getBookByTitle	= //(title)
-library.prototype.getBooksByAuthor	= //(authorName)
-library.prototype.addBooks	= //(books)
-library.prototype.getAuthors	= //()
-library.prototype.getRandomAuthorName	= //()
+library.prototype.addBook = function(){};  // method to add book to array
+library.prototype.removeBookByTitle = function(){};  //(title)
+library.prototype.removeBookByAuthor = function(){};  //(authorName)
+library.prototype.getRandomBook = function(){};  //()
+library.prototype.getBookByTitle = function(){}; //(title)
+library.prototype.getBooksByAuthor = function(){};  //(authorName)
+library.prototype.addBooks = function(){};  //(books)
+library.prototype.getAuthors = function(){};  //()
+library.prototype.getRandomAuthorName = function(){};  //()
+
+//  put instances at bottom per Eric
+var gLibrary = new library();   // global library
+var book1 = new newBook( "The White Dragon","Anne McCaffrey", 365, 1978);   //  FIGURE OUT DATE!!!
+var book2 = new newBook( "The Bourne Identity","Robert Ludlum", 256, 1981); //  FIGURE OUT DATE!!!
